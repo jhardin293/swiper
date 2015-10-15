b040718eebc5dd6a0bb6344a27d1d6706986d3d3
Justin Hardin <jpmitchell293@gmail.com>
2015-10-14 18:01:22 -0700

    init

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/fulltest-dev'
+  },
+
+  seedDB: true
+};
