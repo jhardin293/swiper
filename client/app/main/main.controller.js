681237529154fe61e8184d23cc3030dccfee7a5e
Justin Hardin <jpmitchell293@gmail.com>
2015-10-17 05:30:27 -0700

    score count

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,55 +1,33 @@
 'use strict';
 angular.module('fullTestApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
-    $scope.awesomeThings = [];
     $scope.notes = [];
-    $scope.test = 'test';
-
-    $http.get('/api/things').success(function(awesomeThings) {
-      console.log(awesomeThings);
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
-    });
 
     $http.get('/api/notes').success(function(notes){
       $scope.notes = notes;
-      console.log($scope.notes);
       socket.syncUpdates('note', $scope.notes);
     });
 
-    $scope.clicked = false;
+    var updateCounts = function () {
+      $scope.sure = _.where($scope.notes, { understand: true, throwOut: true });
+      $scope.unsure = _.where($scope.notes, { understand: false , throwOut: true});
+      console.log('sure', $scope.sure.length, 'unsure', $scope.unsure.length);
+    };
+
+    $scope.throwOut = function (note, action) {
+      note.throwOut = true;
+
+      if(action === 'sure') {
+        note.understand = true;
+      }else{
+        note.understand = false;
+      }
+
+      updateCounts();
+    };
+
     $scope.toogleFlip = function (note) {
       note.clicked === true ? note.clicked = false : note.clicked = true;
-      console.log($scope.notes);
-    };
-
-    $scope.getColor = function($index) {
-      var _d = ($index + 1) % 11;
-      var bg = '';
-
-      switch(_d) {
-        case 1:       bg = 'red';         break;
-        case 2:       bg = 'green';       break;
-        case 3:       bg = 'darkBlue';    break;
-        case 4:       bg = 'blue';        break;
-        case 5:       bg = 'yellow';      break;
-        case 6:       bg = 'pink';        break;
-        case 7:       bg = 'darkBlue';    break;
-        case 8:       bg = 'purple';      break;
-        case 9:       bg = 'deepBlue';    break;
-        case 10:      bg = 'lightPurple'; break;
-        default:      bg = 'yellow';      break;
-      }
-
-      return bg;
-    };
-
-    $scope.getSpan = function($index) {
-      var _d = ($index + 1) % 11;
-
-      if (_d === 1 || _d === 5) {
-        return 2;
-      }
     };
 
     $scope.deleteThing = function(thing) {
