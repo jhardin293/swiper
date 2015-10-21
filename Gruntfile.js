d895fe4ebc37541b48c6dd03b427826de7c38d82
Justin Hardin <jpmitchell293@gmail.com>
2015-10-21 15:30:46 -0700

    angular mobile bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -368,6 +368,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '.htaccess',
             'bower_components/**/*',
+            'vendor/**/*',
             'assets/images/{,*/}*.{webp}',
             'assets/fonts/**/*',
             'assets/icons/**/*',
